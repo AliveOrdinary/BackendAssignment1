8ddde93e377595f12f13248ebce3c081725d2531
Navaneeth Krishnan <vrnavaneeth135@gmail.com>
2024-02-02 17:26:13 -0500

    Add project files.

---
+++ BackendAssignment1/Controllers/HostingCostController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackendAssignment1.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+        public string GET (double id)
+        {
+            double charge = 5.50;
+
+            double HST = 0.13;
+
+
+
+            if (id == 0)
+            {
+                int days = 1;
+
+                double cost = charge * days;
+
+                double tax = Math.Round(cost * HST, 2);
+
+                double total = Math.Round(cost + tax,2);
+
+                string result = $"{days} fortnights at $5.50/FN = ${cost} CAD\n HST 13% = ${tax} CAD\n Total = ${total} CAD";
+
+                return result ;
+            }
+            else
+            {
+                double days = Math.Ceiling(id / 14.0);
+
+                double cost = charge * days;
+
+                double tax = Math.Round(cost * HST, 2);
+
+                double total = Math.Round(cost + tax, 2);
+
+                string result = $"{days} fortnights at $5.50/FN = ${cost} CAD\n HST 13% = ${tax} CAD\n Total = ${total} CAD";
+
+                return result;
+            }
+
+            
+
+        }
+    }
+}
